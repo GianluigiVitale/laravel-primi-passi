f4d1f4b2e98e8a44c6ef0f5e14e2f7de595ec874
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-05-04 19:00:09 +0200

    basic homepage with different types of pasta ordered by descend. length
    created scss files, basic styling

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Contatti</title>
+    </head>
+    <body>
+        CONTATTO 1 
+    </body>
+</html>
