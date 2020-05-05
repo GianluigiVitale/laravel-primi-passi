58a7a4942ef47b12ec204d3e364d530ae7c3f643
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-05-05 13:08:35 +0200

    html footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,54 @@
 <footer>
-    
+    <div class="container">
+        <div class="infos">
+            <img id="molisana-logo" src="{{asset('img/logo.png')}}" alt="La Molisana">
+            <p>Ragione sociale: La Molisana S.P.A.</p>
+            <p>Sede legale: Contrada Colle delle Api, 100/A<br> 86100 - Campobasso (CB)</p>
+            <p>Pec: lamolisana@pec.it</p>
+            <p>Tel: +39 0874 4981</p>
+            <p>Fax: +39 0874 629584</p>
+            <p>info@lamolisana.it (per segnalazioni degli utenti)</p>
+            <p>export@lamolisana.it</p>
+            <p>numero verde 800818081</p>
+            <p>telefono 3801292455</p>
+        </div>
+        <div class="pastificio-prodotti list-footer">
+            <div class="footer-column">
+                <h3>PASTIFICIO</h3>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c’è</li>
+                    <li>Filiera Integrata</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                    <li>Spaghetto Quadrato</li>
+                    <li>Le Persone</li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h3>PRODOTTI</h3>
+                <ul>
+                    <li>Le Classiche</li>
+                    <li>Le Integrali</li>
+                    <li>Le Speciali</li>
+                    <li>Le Biologiche</li>
+                    <li>Le Gluten-Free</li>
+                    <li>Le Semole</li>
+                    <li>Le Extra di Lusso</li>
+                </ul>
+            </div>
+        </div>
+        <div class="collezione list-footer">
+            <div class="footer-column">
+                <h3>COLLEZIONE DA CHEF</h3>
+                <ul>
+                    <li>Collezione da Chef</li>
+                    <li>Grandi Cucine</li>
+                    <li>Biologiche</li>
+                    <li>Quadrate</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
