5d99a9791292feab9c5b8da03c2c4be1f80931f5
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-05-06 15:50:16 +0200

    More views and bg, removed important
    -Created home and news view
    -background color on active page
    -added background img to main of product page
    -removed important from font-awesome icon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,10 +2,16 @@
     <img class="molisana-logo" src="{{asset('img/logo.png')}}" alt="La Molisana">
     <nav class="header-nav">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#" class="active">Prodotti</a></li>
-            <li><a href="#">News</a></li>
+            <li>
+                <a href="{{route('home')}}" class="{{(url()->current() == route('home')) ? 'active' : ''}}">Home</a>
+            </li>
+            <li>
+                <a href="{{route('prodotti')}}" class="{{(url()->current() == route('prodotti')) ? 'active' : ''}}">Prodotti</a>
+            </li>
+            <li>
+                <a href="{{route('news')}}" class="{{(url()->current() == route('news')) ? 'active' : ''}}">News</a>
+            </li>
         </ul>
     </nav>
-    <i class="fas fa-bars"></i>
+    <i class="fas fa-bars lg-none"></i>
 </header>
